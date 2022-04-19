966306be56f810c026c100b6733e72a17623fe6c
Eros <erosburastero_it@hotmail.com>
2022-04-18 22:30:14 -0300

    i updated the information

--- src/pages/Lines.vue
+++ src/pages/Lines.vue
@@ -52,7 +52,7 @@
     <v-expansion-panel>
       <v-expansion-panel-header class="black white--text title font-weight-bold">TOOLS</v-expansion-panel-header>
       <v-expansion-panel-content class="grey darken-2 font-weight-black " align="center">
-        NPM/YARN  —  GIT/GIT-FLOW —  GITHUB/GITLAB  — PRETTIER  —  CYPRESS   —  WEBPACK  — BABEL  —  VSCODE  — SCSS/SASS  — ETC...
+        NPM/YARN  —  GIT/GIT-FLOW —  GITHUB/GITLAB  — PRETTIER  —  CYPRESS   —  WEBPACK  — BABEL  —  VSCODE  — SCSS/SASS  —  NETLIFY HOSTING  — ETC...
       </v-expansion-panel-content>
     </v-expansion-panel>
 
